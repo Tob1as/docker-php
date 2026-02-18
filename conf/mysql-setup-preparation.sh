dae4ebffc105edbe54257b8348bd22a50ce62da9
Tobias Hargesheimer <tobias@ison.ws>
2026-02-18 20:06:11 +0100

    Update mysql-setup-preparation.sh
    now MYSQL_ or MARIADB_ ENVs can be used...

--- conf/mysql-setup-preparation.sh
+++ conf/mysql-setup-preparation.sh
@@ -3,7 +3,7 @@
 : "${MYSQL_SETUP_ENABLED:="0"}"                  # set to 1 to enable
 : "${MYSQL_HOST:=""}"                            # set Host of MySQL/MariaDB Server
 : "${MYSQL_PORT:="3306"}"                        # set Port of MySQL/MariaDB Server
-: "${MYSQL_ROOT_PASSWORD:=""}"                   # set MySQL Root Password
+: "${MYSQL_ROOT_PASSWORD:=""}"                   # set MySQL/MariaDB Root Password
 : "${MYSQL_DATABASE:=""}"                        # set Databasename for User
 : "${MYSQL_USER:=""}"                            # set Username
 : "${MYSQL_PASSWORD:=""}"                        # set Password for User
@@ -11,51 +11,62 @@
 : "${MYSQL_EXPORTER_PASSWORD:=""}"               # set Password for Exporter-Username
 : "${MYSQL_EXPORTER_MAXUSERCONNECTIONS:="0"}"    # max connection, set to 0 for unlimited, recommended: 3
 
+: "${MARIADB_SETUP_ENABLED:="$MYSQL_SETUP_ENABLED"}"
+: "${MARIADB_HOST:="$MYSQL_HOST"}"
+: "${MARIADB_PORT:="$MYSQL_PORT"}"
+: "${MARIADB_ROOT_PASSWORD:="$MYSQL_ROOT_PASSWORD"}"
+: "${MARIADB_DATABASE:="$MYSQL_DATABASE"}"
+: "${MARIADB_USER:="$MYSQL_USER"}"
+: "${MARIADB_PASSWORD:="$MYSQL_PASSWORD"}"
+: "${MARIADB_EXPORTER_USER:="$MYSQL_EXPORTER_USER"}"
+: "${MARIADB_EXPORTER_PASSWORD:="$MYSQL_EXPORTER_PASSWORD"}"
+: "${MARIADB_EXPORTER_MAXUSERCONNECTIONS:="$MYSQL_EXPORTER_MAXUSERCONNECTIONS"}"
+
 host='%'  # set '%' to allow from all host
 options='--skip-ssl-verify-server-cert'
 
-if [ "$MYSQL_SETUP_ENABLED" -eq "1" -a -n "$MYSQL_HOST" -a -n "$MYSQL_PORT" -a -n "$MYSQL_ROOT_PASSWORD" ]; then
-    echo ">> MySQL Setup preparation on Host ${MYSQL_HOST} and Port ${MYSQL_PORT} ..."
+if [ "$MARIADB_SETUP_ENABLED" -eq "1" -a -n "$MARIADB_HOST" -a -n "$MARIADB_PORT" -a -n "$MARIADB_ROOT_PASSWORD" ]; then
+    echo ">> MySQL Setup preparation on Host ${MARIADB_HOST} and Port ${MARIADB_PORT} ..."
 
     echo ">> MySQL waiting for Server ..."
-    until mariadb-admin ping -h ${MYSQL_HOST} -P ${MYSQL_PORT} -u root --password="${MYSQL_ROOT_PASSWORD}" ${options} --skip-verbose --silent >/dev/null 2>&1; do
+    until mariadb-admin ping -h ${MARIADB_HOST} -P ${MARIADB_PORT} -u root --password="${MARIADB_ROOT_PASSWORD}" ${options} --skip-verbose --silent >/dev/null 2>&1; do
         sleep 2
     done
     echo ">> MySQL Server ready ..."
 
     # User
-    if [ -n "$MYSQL_USER" -a -n "$MYSQL_PASSWORD" -a -n "$MYSQL_DATABASE" ]; then
-        echo ">> MySQL Database (${MYSQL_DATABASE}) and User (${MYSQL_USER}) ..."
-        mariadb -h ${MYSQL_HOST} -P ${MYSQL_PORT} -u root --password="${MYSQL_ROOT_PASSWORD}" ${options} -sNe \
-        "SELECT user FROM mysql.user WHERE user = '${MYSQL_USER}' GROUP BY user;" \
-        | grep -q ${MYSQL_USER} \
-        || mariadb -h ${MYSQL_HOST} -P ${MYSQL_PORT} -u root --password="${MYSQL_ROOT_PASSWORD}" ${options} -sN <<EOSQL
-        CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};
-        CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'${host}' IDENTIFIED BY '${MYSQL_PASSWORD}';
-        GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'${host}';
+    if [ -n "$MARIADB_USER" -a -n "$MARIADB_PASSWORD" -a -n "$MARIADB_DATABASE" ]; then
+        echo ">> MySQL Database (${MARIADB_DATABASE}) and User (${MARIADB_USER}) ..."
+        mariadb -h ${MARIADB_HOST} -P ${MARIADB_PORT} -u root --password="${MARIADB_ROOT_PASSWORD}" ${options} -sNe \
+        "SELECT user FROM mysql.user WHERE user = '${MARIADB_USER}' GROUP BY user;" \
+        | grep -q ${MARIADB_USER} \
+        || mariadb -h ${MARIADB_HOST} -P ${MARIADB_PORT} -u root --password="${MARIADB_ROOT_PASSWORD}" ${options} -sN <<EOSQL
+        CREATE DATABASE IF NOT EXISTS ${MARIADB_DATABASE};
+        CREATE USER IF NOT EXISTS '${MARIADB_USER}'@'${host}' IDENTIFIED BY '${MARIADB_PASSWORD}';
+        GRANT ALL PRIVILEGES ON ${MARIADB_DATABASE}.* TO '${MARIADB_USER}'@'${host}';
         FLUSH PRIVILEGES;
 EOSQL
     else
-        echo ">> MySQL Database (${MYSQL_DATABASE}) and User (${MYSQL_USER}) skipped."
+        echo ">> MySQL Database (${MARIADB_DATABASE}) and User (${MARIADB_USER}) skipped."
     fi
 
     # Exporter
-    if [ -n "$MYSQL_EXPORTER_USER" -a -n "$MYSQL_EXPORTER_PASSWORD" -a -n "$MYSQL_EXPORTER_MAXUSERCONNECTIONS" ]; then
-        echo ">> MySQL Exporter-User (${MYSQL_EXPORTER_USER}) ..."
-        mariadb -h ${MYSQL_HOST} -P ${MYSQL_PORT} -u root --password="${MYSQL_ROOT_PASSWORD}" ${options} -sNe \
-        "SELECT user FROM mysql.user WHERE user = '${MYSQL_EXPORTER_USER}' GROUP BY user;" \
-        | grep -q ${MYSQL_EXPORTER_USER} \
-        || mariadb -h ${MYSQL_HOST} -P ${MYSQL_PORT} -u root --password="${MYSQL_ROOT_PASSWORD}" ${options} -sN <<EOSQL
-        CREATE USER IF NOT EXISTS '${MYSQL_EXPORTER_USER}'@'${host}' IDENTIFIED BY '${MYSQL_EXPORTER_PASSWORD}' WITH MAX_USER_CONNECTIONS ${MYSQL_EXPORTER_MAXUSERCONNECTIONS};
-        GRANT PROCESS, REPLICATION CLIENT, SELECT ON *.* TO '${MYSQL_EXPORTER_USER}'@'${host}';
+    if [ -n "$MARIADB_EXPORTER_USER" -a -n "$MARIADB_EXPORTER_PASSWORD" -a -n "$MARIADB_EXPORTER_MAXUSERCONNECTIONS" ]; then
+        echo ">> MySQL Exporter-User (${MARIADB_EXPORTER_USER}) ..."
+        mariadb -h ${MARIADB_HOST} -P ${MARIADB_PORT} -u root --password="${MARIADB_ROOT_PASSWORD}" ${options} -sNe \
+        "SELECT user FROM mysql.user WHERE user = '${MARIADB_EXPORTER_USER}' GROUP BY user;" \
+        | grep -q ${MARIADB_EXPORTER_USER} \
+        || mariadb -h ${MARIADB_HOST} -P ${MARIADB_PORT} -u root --password="${MARIADB_ROOT_PASSWORD}" ${options} -sN <<EOSQL
+        CREATE USER IF NOT EXISTS '${MARIADB_EXPORTER_USER}'@'${host}' IDENTIFIED BY '${MARIADB_EXPORTER_PASSWORD}' WITH MAX_USER_CONNECTIONS ${MARIADB_EXPORTER_MAXUSERCONNECTIONS};
+        GRANT PROCESS, REPLICATION CLIENT, SELECT ON *.* TO '${MARIADB_EXPORTER_USER}'@'${host}';
         FLUSH PRIVILEGES;
 EOSQL
     else
-        echo ">> MySQL Exporter-User (${MYSQL_EXPORTER_USER}) skipped."
+        echo ">> MySQL Exporter-User (${MARIADB_EXPORTER_USER}) skipped."
     fi
 
     # checks
     echo ">> MySQL checks:"
-    mariadb -h ${MYSQL_HOST} -P ${MYSQL_PORT} -u root --password="${MYSQL_ROOT_PASSWORD}" ${options} -e 'SELECT user, host, max_user_connections FROM mysql.user;'
-    mariadb -h ${MYSQL_HOST} -P ${MYSQL_PORT} -u root --password="${MYSQL_ROOT_PASSWORD}" ${options} -e 'SELECT host, user, db FROM mysql.db;'
+    mariadb -h ${MARIADB_HOST} -P ${MARIADB_PORT} -u root --password="${MARIADB_ROOT_PASSWORD}" ${options} -e 'SELECT user, host, max_user_connections FROM mysql.user;'
+    mariadb -h ${MARIADB_HOST} -P ${MARIADB_PORT} -u root --password="${MARIADB_ROOT_PASSWORD}" ${options} -e 'SELECT host, user, db FROM mysql.db;'
 fi
